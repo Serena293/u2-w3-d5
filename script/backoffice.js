40038cb9b592b8b839c239c7c10164a070672cf5
SERENA FERRARO <serenaferraro21@gmail.com>
2024-11-15 14:39:34 +0100

    POST

--- script/backoffice.js
+++ script/backoffice.js
@@ -1,48 +1,53 @@
-const form = document.getElementById('formNewProduct');
-const newProduct = document.getElementById('newProduct');
-const price = document.getElementById('price');
-const description = document.getElementById('description');
-const imgURL = document.getElementById('imgURL');
-const saveBtn = document.getElementById('save-btn');
+const form = document.getElementById("formNewProduct");
+const newProduct = document.getElementById("newProduct");
+const price = document.getElementById("price");
+const description = document.getElementById("description");
+const imgURL = document.getElementById("imgURL");
+const saveBtn = document.getElementById("save-btn");
 
 class Product {
-    constructor(_product, _price, _description, _imgURL, _brand) {
-        this.nameame = _product;
-        this.price = _price;
-        this.description = _description;
-        this.ImageURL = _imgURL;
-        this.band = _brand
-    }
+  constructor(_product, _price, _description, _imgURL, _brand) {
+    this.name = _product;
+    this.description = _description;
+    this.imageUrl = _imgURL;
+    this.price = _price;    
+    this.brand = _brand;
+  }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
 
-    const product = new Product(
-        newProduct.value, 
-        price.value, 
-        description.value, 
-        imgURL.value,
-        'Clementoni'
-    );
 
-    fetch("https://striveschool-api.herokuapp.com/api/product/", {
-        method: 'POST', 
-        body: JSON.stringify(product),
-        headers: {
-            'Content-type': 'application/json',
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MDJmOThhZDEyOTAwMTU4NzZiYmIiLCJpYXQiOjE3MzE2NzQ1NDIsImV4cCI6MTczMjg4NDE0Mn0.cRzSYwMnPBznRXqX9JpKOHbyezRsvZUeUl2bd43I75A"
-        }
-    })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const product = new Product(
+    newProduct.value,
+    price.value,
+    description.value,
+    imgURL.value,
+    "Clementoni"
+  );
+
+  fetch("https://striveschool-api.herokuapp.com/api/product/", {
+    method: "POST",
+    body: JSON.stringify(product),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTg4MDhhZDEyOTAwMTU4NzZiZTUiLCJpYXQiOjE3MzE2NjQxODUsImV4cCI6MTczMjg3Mzc4NX0.-avXl_JYeNix_3ymyoTPi6_dIHMLKGx7Dtd2TCxpBB8",
+    },
+  })
     .then((response) => {
-        if (response.ok) {
-            alert('Prodotto salvato');
-        } else {
-            throw new Error('Errore');
-        }
+      if (response.ok) {
+        // console.log(response)
+        alert("Prodotto salvato");
+      } else {
+        throw new Error("Errore");
+      }
     })
     .catch((error) => {
-        console.log('errore', error);
-        alert('Errore salvataggio');
+      console.log("errore", error);
+      alert("Errore salvataggio");
     });
 });
